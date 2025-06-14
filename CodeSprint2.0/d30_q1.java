79cc86fa572ac06a0b0ed700e7a0335dbfe839bc
Sessobin <mohantykrishna57@gmail.com>
2025-06-15 00:26:00 +0530

    30

--- CodeSprint2.0/d30_q1.java
+++ CodeSprint2.0/d30_q1.java
@@ -1,3 +1,25 @@
-public class d30_q1 {
-    
+import java.util.*;
+
+public class d30_q1{
+  public static int countSlips(String path){
+    Set<String> visited =new HashSet<>();
+    int x=0,y=0;
+    visited.add("0,0");
+    int slips=0;
+    for(char move:path.toCharArray()){
+        if(move=='L')x--;
+        else if(move=='R')x++;
+        else if(move=='U')y++;
+        else if(move =='D')y--;
+        String pos=x+","+y;
+          if(visited.contains(pos)){
+            slips++;
+          }else{
+            visited.add(pos);
+          }
+    }return slips;
+  }
+  public static void main(String args[]){
+    System.out.println(countSlips(args[0]));
+  }
 }
--- CodeSprint2.0/d30_q2.java
+++ CodeSprint2.0/d30_q2.java
@@ -1,3 +1,24 @@
-public class d30_q2 {
-    
+import java.util.*;
+public class d30_q2{
+  public static int minMana(int[] merlin,int[] morgana){
+    int maxMorgana=Integer.MIN_VALUE;
+    for(int power:morgana){
+      maxMorgana=Math.max(maxMorgana,power);
+    }int mana=0;
+    for(int power : merlin){
+      if(power <=maxMorgana){
+        mana+=(maxMorgana+1-power);
+      }
+    }return mana;
+  }
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] merlin=new int[n];
+    for(int i=0;i<n;i++)merlin[i]=sc.nextInt();
+    int m=sc.nextInt();
+    int[] morgana=new int[m];
+    for(int i=0;i<m;i++)morgana[i]=sc.nextInt();
+    System.out.println(minMana(merlin,morgana));
+  }
 }
